970af2c90f29a2b2c4be64cc14414fb40249acc8
poctob <alexp@xpresstek.net>
2015-08-19 14:31:37 -0400

    local trial, growl

--- config/mail.php
+++ config/mail.php
@@ -80,7 +80,7 @@ return [
 	|
 	*/
 
-	'username' => env('MAIL_USERNAME','postmaster@mg.xpresstek.net'),
+	'username' => env('MAIL_USERNAME'),
 
 	/*
 	|--------------------------------------------------------------------------
@@ -93,7 +93,7 @@ return [
 	|
 	*/
 
-	'password' => env('MAIL_PASSWORD','b969a569c98d0f378e08f61b7bdddb20'),
+	'password' => env('MAIL_PASSWORD'),
 
 	/*
 	|--------------------------------------------------------------------------
